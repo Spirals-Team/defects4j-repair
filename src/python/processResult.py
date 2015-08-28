b1ed66be0e8f348b1445db03aba1d923ed1a0a0b
Thomas Durieux <durieuxthomas@gmail.com>
2015-08-28 17:09:54 +0200

    Update the readme script

--- src/python/processResult.py
+++ src/python/processResult.py
@@ -7,12 +7,20 @@ import sys
 import datetime
 from core.Config import conf
 
-tools = ["NopolPC", "NopolC", "Genprog", "Kali"]
+from core.projects.LangProject import LangProject
+from core.projects.MathProject import MathProject
+from core.projects.ChartProject import ChartProject
+from core.projects.TimeProject import TimeProject
 
 reload(sys)  
 sys.setdefaultencoding('Cp1252')
 
+tools = ["NopolPC", "NopolC", "Genprog", "Kali"]
 
+ignored = {
+	"Math": [99],
+	"Chart": [8]
+}
 root = conf.resultsRoot
 resultsAll = {}
 resultsAllPath = os.path.join(root, "results.json")
@@ -29,9 +37,9 @@ totalBugs = 0
 totalExecutionTime = 0
 
 count = {}
-result = "# Summary\n\n"
+result = "# All fixed bugs\n\n"
 body = ""
-line = "BugId             | "
+line = " #   | BugId             | "
 texTable = """\\begin{table}[!t]
 \label{tab:bugs_summary}
 \centering
@@ -40,16 +48,49 @@ texTable = """\\begin{table}[!t]
 \\begin{tabular}{|c|c|c|c|c|c|}
 \hline 
 Bug id            & """
-for tool in tools:
-	if tool == "Ranking":
-		continue
-	line += "{0:9} | ".format(tool)
-	texTable += "{0:9} & ".format(tool)
 
-tableHeader = "%sTotal\n----------------- | " % line
+def projectToObj(project):
+	if project == "Math":
+		return MathProject()
+	if project == "Lang":
+		return LangProject()
+	if project == "Time":
+		return TimeProject()
+	if project == "Chart":
+		return ChartProject()
+def getToolName(tool):
+	if tool == "Ranking":
+		return None
+	elif tool == "BrutpolC" and "BrutpolPC" in tools:
+		return None
+	elif tool == "NopolC" and "NopolPC" in tools:
+		return None
+	elif tool == "BrutpolPC" and "BrutpolC" in tools:
+		tool = "Brutpol"
+	elif tool == "NopolPC" and "NopolC" in tools:
+		tool = "Nopol"
+	elif tool == "Genprog":
+		tool = "jGenProg"
+	elif tool == "Kali":
+		tool = "jKali"
+	return tool
+def getToolsHeader(tools, separator = "|"):
+	results = ""
+	for tool in sorted(tools):
+		tool = getToolName(tool)
+		if tool == None:
+			continue
+		results += "{0:9} {1:1} ".format(tool, separator)
+	return results
+
+line += getToolsHeader(tools, "|")
+texTable += getToolsHeader(tools, "&")
+
+tableHeader = "%sTotal\n---- | ----------------- | " % line
 texTable += "Total \\\\\n\\hline\n"
 for tool in tools:
-	if tool == "Ranking":
+	tool = getToolName(tool)
+	if tool == None:
 		continue
 	tableHeader += "--------- | "
 tableHeader += "------\n"
@@ -60,10 +101,19 @@ fullTable = tableHeader
 simpleTable = tableHeader
 body = ""
 print("Start the output proccess")
+bugCount = 0
+patchCount = 0
+commits = {}
+
+pathCommitId = os.path.join(os.path.dirname(__file__),'data/commitID.json')
+with open(pathCommitId) as data_file:
+	commits = json.load(data_file)
+
 for project in sorted(os.listdir(root)):
 	projectPath = os.path.join(root, project) 
 	if os.path.isfile(projectPath):
 		continue
+	projectobj = projectToObj(project)
 	smallName = project[0]
 	resultsProject = {}
 	rankingBug = {}
@@ -72,6 +122,7 @@ for project in sorted(os.listdir(root)):
 		if not bugId.isdigit() or os.path.isfile(bugPath):
 			continue
 		bugId = int(bugId)
+		bugCount += 1
 
 		sys.stdout.write("\033[F")
 		sys.stdout.write("\033[K")
@@ -80,54 +131,105 @@ for project in sorted(os.listdir(root)):
 		ranking = None
 
 		totalBugs += 1
-		line = "{0:17} |"
+		line = " {0:3} | {1:17} |"
 		lineArgs = []
 		texLineArgs = []
 		texLineTable = "{0:17} &"
-		lineArgs += ["[%s%d](#%s-%s)" % (smallName, bugId, project.lower(), bugId)]
+		lineArgs += [bugCount, "[%s%d](#%s-%s)" % (smallName, bugId, project.lower(), bugId)]
 		texLineArgs += ["%s%d"  % (smallName, bugId)]
 		lineCount = 0
 
 		resultsBugPath = os.path.join(bugPath, "results.json")
 		resultsBug = {}
-		for (index, tool) in enumerate(tools):
-			if tool == "Ranking":
-				continue
-			toolPath = os.path.join(bugPath, tool)
-			if os.path.isfile(toolPath):
-				continue
-			line += " {%d:9} |" % (index + 1)
-			texLineTable +=  " {%d:9} &" % (index + 1)
-			resultsToolPath = os.path.join(toolPath, "results.json")
-			stderrToolPath = os.path.join(toolPath, "stderr.log")
-			stdoutToolPath = os.path.join(toolPath, "stdout.log")
-			if os.path.exists(resultsToolPath):
-				with open(resultsToolPath) as data_file:
-					resultsBug[tool] = json.load(data_file)
-			elif os.path.exists(stderrToolPath):
-				with open(stderrToolPath) as data_file:
-					data = data_file.read()
-					m = re.search("Job [0-9]+ KILLED", data)
-					if m:
-						resultsBug[tool] = {
-							"error": "TIMEOUT"
-						}
-						nbTimeout += 1
-					elif len(data) == 0:
-						resultsBug[tool] = {
-							"error": "EMPTY"
-						}
-						nbEmpty += 1
-					else:
-						resultsBug[tool] = {
-							"error": "ERROR"
-						}
-						nbError += 1
-			else:
-				lineArgs += [""]
-				texLineArgs += [""]
-				continue
 
+		if project in ignored and bugId in ignored[project]:
+			for (index, tool) in enumerate(tools):
+				tool = getToolName(tool)
+				if tool == None:
+					continue
+				resultsBug[tool] = {
+					"error": "IGNORED"
+				}
+		else:
+			for (index, tool) in enumerate(tools):
+				if tool == "Ranking":
+					continue
+				toolPath = os.path.join(bugPath, tool)
+				if os.path.isfile(toolPath):
+					continue
+				resultsToolPath = os.path.join(toolPath, "results.json")
+				stderrToolPath = os.path.join(toolPath, "stderr.log")
+				stdoutToolPath = os.path.join(toolPath, "stdout.log")
+				if os.path.exists(resultsToolPath):
+					with open(resultsToolPath) as data_file:
+						if (tool == "NopolC" and 
+							"Nopol" in resultsBug and 
+							"patch" in resultsBug["Nopol"] and 
+							resultsBug["Nopol"]["patch"] is not None):
+							continue
+						if (tool == "BrutpolC" and 
+							"Brutpol" in resultsBug and 
+							"patch" in resultsBug["Brutpol"] and 
+							resultsBug["Brutpol"]["patch"] is not None):
+							continue
+						
+						values = json.load(data_file)
+						if tool == "BrutpolC":
+							tool = "Brutpol"
+						elif tool == "NopolC":
+							tool = "Nopol"
+						tool = getToolName(tool)
+						# ignore the results Math 44 and Math 56
+						if (bugId == 44 or bugId == 56) and project == "Math":
+							values["operations"] = []
+						resultsBug[tool] = values
+				elif os.path.exists(stderrToolPath):
+					if ("Nopol" in tool and 
+						"Nopol" in resultsBug):
+						continue
+					if ( "Brutpol" in tool and 
+						"Brutpol" in resultsBug):
+						continue
+					if tool == "BrutpolC":
+						tool = "Brutpol"
+					elif tool == "NopolC":
+						tool = "Nopol"
+					tool = getToolName(tool)
+					with open(stderrToolPath) as data_file:
+						data = data_file.read()
+						m = re.search("Job [0-9]+ KILLED", data)
+						if m:
+							resultsBug[tool] = {
+								"error": "TIMEOUT"
+							}
+						elif len(data) == 0:
+							resultsBug[tool] = {
+								"error": "EMPTY"
+							}
+						else:
+							resultsBug[tool] = {
+								"error": "ERROR"
+							}
+							
+				else:
+					lineArgs += [""]
+					texLineArgs += [""]
+					continue
+		index = 0
+		for tool in sorted(tools):
+			tool = getToolName(tool)
+			if tool == None:
+				continue
+			if "error" in resultsBug[tool]:
+				if "EMPTY" in resultsBug[tool]["error"]:
+					nbEmpty += 1
+				elif "TIMEOUT" in resultsBug[tool]["error"]:
+					nbTimeout += 1
+				elif "ERROR" in resultsBug[tool]["error"]:
+					nbError += 1
+			index += 1
+			line += " {%d:9} |" % (index + 1)
+			texLineTable +=  " {%d:9} &" % (index)
 			if 'executionTime' in resultsBug[tool] and resultsBug[tool]['executionTime']:
 				totalExecutionTime += resultsBug[tool]['executionTime']
 			elif 'timeTotal' in resultsBug[tool]:
@@ -153,7 +255,9 @@ for project in sorted(os.listdir(root)):
 					path = os.path.join(conf.defects4jRoot, "framework/projects", project, "patches",  "%d.src.patch" % bugId)
 					with open(path) as patchFile:
 						content = patchFile.read()
+						content = re.sub(re.compile(r"^& ", re.IGNORECASE + re.M), "- ", re.sub(re.compile(r"^- ", re.IGNORECASE + re.M), "+ ", re.sub(re.compile(r"^\+ ", re.IGNORECASE + re.M), "& ", content)))
 						body += "```Java\n%s\n```\n\n" % (content)
+				patchCount += 1
 				lineArgs += ["Yes"]
 				texLineArgs += ["Fixed"]
 				lineCount += 1
@@ -162,33 +266,47 @@ for project in sorted(os.listdir(root)):
 				else:
 					count[tool] += 1
 
-				body += "## %s \n\n" % (tool)
+				def getPatchLocation(className, line):
+					if project in commits and str(bugId) in commits[project]:
+						source = projectobj.getSource(bugId)
+						repo = projectobj.data["url"]
+						commitID = commits[project][str(bugId)]
+						classPath = className.replace(".", "/")
+						return "[%s:%s](%s/blob/%s/%s/%s.java#L%s)" % (className, line,repo, commitID, source, classPath, line)
+					return "%s:%s" % (className, line)
+				body += "## Patch #%d %s \n\n" % (patchCount, tool)
 				if "operations" in resultsBug[tool]:
-					for operation in resultsBug[tool]["operations"]:
+					for operation in resultsBug[tool]["operations"][0:1]:
 						lineKey = "%s:%d" % (operation['patchLocation']["className"], operation['patchLocation']["line"])
+						pathLocation = getPatchLocation(operation['patchLocation']["className"],operation['patchLocation']["line"])
 						if 'suspicious' in ranking and lineKey in ranking['suspicious']:
 							ranks = ranking['suspicious'][lineKey]['rank']
-							body += "%s (Suspicious rank: " % (lineKey)
+							body += "%s (Suspicious rank: " % (pathLocation)
 							for rank in ranks:
 								body += "%s %d, " % (rank, ranks[rank])
 							body += ")\n"
 						else:
-							body += "%s\n" % (lineKey)
+							body += "%s\n" % (pathLocation)
 						if 'type' in operation:
-							body+= operation["type"] + "\n"
-						body += "```Java\n%s\n```\n\n" % (operation["patch"])
+							body+= "\nPatch type: %s \n " % operation["type"]
+						body += "\n```Java\n%s\n```\n\n" % (operation["patch"])
 				else:
 					lineKey = "%s:%d" % (resultsBug[tool]['patchLocation']["className"], resultsBug[tool]['patchLocation']["line"])
+					pathLocation = getPatchLocation(resultsBug[tool]['patchLocation']["className"],resultsBug[tool]['patchLocation']["line"])
 					if 'suspicious' in ranking and lineKey in ranking['suspicious']:
 						ranks = ranking['suspicious'][lineKey]['rank']
-						body += "%s (Suspicious rank: " % (lineKey)
+						body += "%s (Suspicious rank: " % (pathLocation)
 						for rank in ranks:
 							body += "%s %d, " % (rank, ranks[rank])
 						body += ")\n"
 					else:
-						body += "%s\n" % (lineKey)
-					body += "```Java\n%s\n```\n\n" % (resultsBug[tool]["patch"])
-					body += "Nb Angelic value: %d\n\n" % resultsBug[tool]['nbAngelicValue']
+						body += "%s\n" % (pathLocation)
+					body+= "\nPatch type: %s" % resultsBug[tool]["patchType"]
+					patch = resultsBug[tool]["patch"]
+					if(resultsBug[tool]["patchType"] == "PRECONDITION"):
+						patch = "if(%s)" % patch
+					body += "\n```Java\n%s\n```\n\n" % (patch)
+					body += "Nb Angelic value: %s\n\n" % resultsBug[tool]['nbAngelicValue']
 					body += "Nb analyzed Statement: %d\n\n" % resultsBug[tool]['nbStatement']
 				if 'executionTime' in resultsBug[tool]:
 					body += "Execution time: %s\n\n" % datetime.timedelta(milliseconds=resultsBug[tool]['executionTime'])
@@ -197,18 +315,21 @@ for project in sorted(os.listdir(root)):
 
 
 			elif ("error" in resultsBug[tool]):
-				lineArgs += [resultsBug[tool]["error"]]
+				if resultsBug[tool]["error"] != "TIMEOUT":
+					lineArgs += [resultsBug[tool]["error"]]
+				else:
+					lineArgs += ["No"]
 				texLineArgs += ["--"]
 			else:
 				if "nbAngelicValue" in resultsBug[tool]:
-					lineArgs += ["%d AV" % resultsBug[tool]["nbAngelicValue"]]
+					lineArgs += ["%s AV" % resultsBug[tool]["nbAngelicValue"]]
 				elif "RegressionTestCases" in resultsBug[tool]:
 					lineArgs += ["%s Reg" % resultsBug[tool]["RegressionTestCases"]]
 				else:
 					lineArgs += ["No"]
 				texLineArgs += ["--"]
-		line += "{%d:7}" % (len(tools) + 1)
-		texLineTable +=  "{%d:7} \\\\" % (len(tools) + 1)
+		line += "{%d:7}" % (index + 2)
+		texLineTable +=  "{%d:7} \\\\" % (index + 1)
 		lineArgs += [lineCount]
 		if lineCount > 0:
 			texLineArgs += ["Fixed"]
@@ -221,11 +342,12 @@ for project in sorted(os.listdir(root)):
 
 	texTable += "\hline\n"
 
-totalLine = "{0:17} | ".format("Total")
+totalLine = "     | {0:17} | ".format("Total")
 texLineTable = "{0:17} & ".format("Total")
 total = 0
-for tool in tools:
-	if tool == "Ranking":
+for tool in sorted(tools):
+	tool = getToolName(tool)
+	if tool == None:
 		continue
 	value = 0
 	percent = 0
@@ -251,8 +373,8 @@ texTable += """\hline
 """
 result += totalLine
 resultSimple += totalLine
-result += "# Complete data\n\n"
-resultSimple += "# Complete data\n\n"
+result += "# All bugs\n\n"
+resultSimple += "# All bugs\n\n"
 print result + fullTable + totalLine
 
 resultsAllFile = open(detailedResultsMdPath, "w")
