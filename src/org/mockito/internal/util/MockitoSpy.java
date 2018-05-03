0a980d911bf3898148597ff2a472bd96c4ee3295
Martin Monperrus <martin.monperrus@gnieh.org>
2018-05-03 10:38:25 +0200

    add defects4j buggy version

---
+++ src/org/mockito/internal/util/MockitoSpy.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockito.internal.util;
+
+/**
+ * Mark internally a MockitoSpy.
+ *
+ * To be used un conjunction with {@link MockUtil#isMock(Object)}
+ */
+public interface MockitoSpy {
+}
