4a6bd06de229ae99ef6d64faf0c9a5f513f5e852
Martin Monperrus <martin.monperrus@gnieh.org>
2018-05-03 09:30:45 +0200

    add defects4j buggy version

---
+++ contrib/externs/chrome_extensions.js
@@ -0,0 +1,510 @@
+/*
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Definitions for the Chromium extensions API.
+*
+*
+ */
+
+/** namespace */
+var chrome = {};
+
+/** @see http://code.google.com/chrome/extensions/extension.html */
+chrome.extension = {};
+
+/**
+ * @param {string|Object.<string>=} opt_arg1 Either the extensionId to
+ *     to connect to, in which case connectInfo params can be passed in the
+ *     next optional argument, or the connectInfo params.
+ * @param {Object.<string>=} opt_connectInfo The connectInfo object,
+ *     if arg1 was the extensionId to connect to.
+ * @return {Port} New port.
+ */
+chrome.extension.connect = function(opt_arg1, opt_connectInfo) {};
+
+/**
+ * @return {Window} The global JS object for the background page.
+ */
+chrome.extension.getBackgroundPage = function() {};
+
+/**
+ * @param {number} opt_windowId An optional windowId.
+ * @return {Array.<Window>} The global JS objects for each content view.
+ */
+chrome.extension.getExtensionTabs = function(opt_windowId) {};
+
+/**
+ * @param {string} path A path to a resource within an extension expressed
+ *     relative to it's install directory.
+ * @return {string} The fully-qualified URL to the resource.
+ */
+chrome.extension.getURL = function(path) {};
+
+/**
+ * @return {Array.<Window>} The global JS objects for each content view.
+ */
+chrome.extension.getViews = function() {};
+
+/**
+ * @param {number|*} opt_arg1 Either the extensionId to send the request to,
+ *     in which case the request is passed as the next arg, or the request.
+ * @param {*} opt_request The request value, if arg1 was the extensionId.
+ * @param {function(*) : void=} opt_callback The callback function which
+ *     takes a JSON response object sent by the handler of the request.
+ */
+chrome.extension.sendRequest = function(opt_arg1, opt_request, opt_callback) {};
+
+/** @type {ChromeEvent} */
+chrome.extension.onConnect;
+/** @type {ChromeEvent} */
+chrome.extension.onConnectExternal;
+/** @type {ChromeEvent} */
+chrome.extension.onRequest;
+/** @type {ChromeEvent} */
+chrome.extension.onRequestExternal;
+
+
+/** @see http://code.google.com/chrome/extensions/tabs.html */
+chrome.tabs = {};
+
+/**
+ * @param {number?} windowId Window Id.
+ * @param {function(string) : void} callback Callback function which accepts
+ *    the data URL string of a JPEG encoding of the visible area of the
+ *    captured tab. May be assigned to the 'src' property of an HTML Image
+ *    element for display.
+ */
+chrome.tabs.captureVisibleTab = function(windowId, callback) {};
+
+/**
+ * @param {number} tabId Tab Id.
+ * @param {Object.<string>=} opt_connectInfo Info Object.
+ */
+chrome.tabs.connect = function(tabId, opt_connectInfo) {};
+
+/**
+ * @param {Object} createProperties Info object.
+ * @param {function(Tab) : void=} opt_callback The callback function.
+ */
+chrome.tabs.create = function(createProperties, opt_callback) {};
+
+/**
+ * @param {number?} tabId Tab id.
+ * @param {function(string) : void} callback Callback function.
+ */
+chrome.tabs.detectLanguage = function(tabId, callback) {};
+
+/**
+ * @param {number?} tabId Tab id.
+ * @param {Object?} details An object which may have 'code', 'file',
+ *    or 'allFrames' keys.
+ * @param {function() : void=} opt_callback Callback function.
+ */
+chrome.tabs.executeScript = function(tabId, details, opt_callback) {};
+
+/**
+ * @param {number} tabId Tab id.
+ * @param {function(Array.<Tab>) : void} callback Callback.
+ */
+chrome.tabs.get = function(tabId, callback) {};
+
+/**
+ * @param {number?} windowId Window id.
+ * @param {function(Array.<Tab>) : void} callback Callback.
+ */
+chrome.tabs.getAllInWindow = function(windowId, callback) {};
+
+/**
+ * @param {number?} windowId Window id.
+ * @param {function(Tab) : void} callback Callback.
+ */
+chrome.tabs.getSelected = function(windowId, callback) {};
+
+/**
+ * @param {number?} tabId Tab id.
+ * @param {Object?} details An object which may have 'code', 'file',
+ *     or 'allFrames' keys.
+ * @param {function() : void=} opt_callback Callback function.
+ */
+chrome.tabs.insertCSS = function(tabId, details, opt_callback) {};
+
+/**
+ * @param {number} tabId Tab id.
+ * @param {Object.<string, number>} moveProperties An object with 'index'
+ *     and optional 'windowId' keys.
+ * @param {function(Tab) : void} opt_callback Callback.
+ */
+chrome.tabs.move = function(tabId, moveProperties, opt_callback) {};
+
+/**
+ * @param {number} tabId Tab id.
+ * @param {function(Tab) : void} opt_callback Callback.
+ */
+chrome.tabs.remove = function(tabId, opt_callback) {};
+
+/**
+ * @param {number} tabId Tab id.
+ * @param {*} request The request value of any type.
+ * @param {function(*) : void=} opt_callback The callback function which
+ *     takes a JSON response object sent by the handler of the request.
+ */
+chrome.tabs.sendRequest = function(tabId, request, opt_callback) {};
+
+/**
+ * @param {number} tabId Tab id.
+ * @param {Object.<string, (string|boolean)>} updateProperties An object which
+ *     may have 'url' or 'selected' key.
+ * @param {function(Tab) : void} opt_callback Callback.
+ */
+chrome.tabs.update = function(tabId, updateProperties, opt_callback) {};
+
+/** @type {ChromeEvent} */
+chrome.tabs.onAttached;
+/** @type {ChromeEvent} */
+chrome.tabs.onCreated;
+/** @type {ChromeEvent} */
+chrome.tabs.onDetached;
+/** @type {ChromeEvent} */
+chrome.tabs.onMoved;
+/** @type {ChromeEvent} */
+chrome.tabs.onRemoved;
+/** @type {ChromeEvent} */
+chrome.tabs.onSelectionChanged;
+/** @type {ChromeEvent} */
+chrome.tabs.onUpdated;
+
+
+/** @see http://code.google.com/chrome/extensions/windows.html */
+chrome.windows = {};
+
+/**
+ * @param {Object?} createData May have 'url', 'left', 'top',
+ *     'width', or 'height' properties.
+ * @param {function(ChromeWindow) : void=} opt_callback Callback.
+ */
+chrome.windows.create = function(createData, opt_callback) {};
+
+/**
+ * @param {Object.<string, boolean>?} getInfo May have 'populate' key.
+ * @param {function(Array.<ChromeWindow>): void} callback Callback.
+ */
+chrome.windows.getAll = function(getInfo, callback) {};
+
+/**
+ * @param {function(ChromeWindow) : void} callback Callback.
+ */
+chrome.windows.getCurrent = function(callback) {};
+
+/**
+ * @param {function(ChromeWindow) : void} callback Callback.
+ */
+chrome.windows.getLastFocused = function(callback) {};
+
+/**
+ * @param {number} tabId Tab Id.
+ * @param {function() : void} opt_callback Callback.
+ */
+chrome.windows.remove = function(tabId, opt_callback) {};
+
+/**
+ * @param {number} tabId Tab Id.
+ * @param {Object.<string, number>} updateProperties An object which may
+ *     have 'left', 'top', 'width', or 'height' keys.
+ * @param {function() : void} opt_callback Callback.
+ */
+chrome.windows.update = function(tabId, updateProperties, opt_callback) {};
+
+/** @type {ChromeEvent} */
+chrome.windows.onCreated;
+/** @type {ChromeEvent} */
+chrome.windows.onFocusChanged;
+/** @type {ChromeEvent} */
+chrome.windows.onRemoved;
+
+/** @see http://code.google.com/chrome/extensions/i18n.html */
+chrome.i18n = {};
+
+/**
+ * @param {function(Array.<string>) : void} callback The callback function which
+ *     accepts an array of the accept languages of the browser, such as
+ *     'en-US','en','zh-CN'.
+ */
+chrome.i18n.getAcceptLanguages = function(callback) {};
+
+/**
+ * @param {string} messageName
+ * @param {(string|Array.<string>)=} opt_args
+ * @return {string}
+ */
+chrome.i18n.getMessage = function(messageName, opt_args) {};
+
+
+
+/** @see http://code.google.com/chrome/extensions/pageAction.html */
+chrome.pageAction = {};
+
+/**
+ * @param {number} tabId Tab Id.
+ */
+chrome.pageAction.hide = function(tabId) {};
+
+/**
+ * @param {number} tabId Tab Id.
+ */
+chrome.pageAction.show = function(tabId) {};
+
+/**
+ * @param {Object} details An object which has 'tabId' and either
+ *     'imageData' or 'path'.
+ */
+chrome.pageAction.setIcon = function(details) {};
+
+/**
+ * @param {Object} details An object which has 'tabId' and 'title'.
+ */
+chrome.pageAction.setTitle = function(details) {};
+
+/** @type {ChromeEvent} */
+chrome.pageAction.onClicked;
+
+
+/** @see http://code.google.com/chrome/extensions/browserAction.html */
+chrome.browserAction = {};
+
+/**
+ * @param {Object} details An object whose keys are 'color' and
+ *     optionally 'tabId'.
+ */
+chrome.browserAction.setBadgeBackgroundColor = function(details) {};
+
+/**
+ * @param {Object} details An object whose keys are 'text' and
+ *     optionally 'tabId'.
+ */
+chrome.browserAction.setBadgeText = function(details) {};
+
+/**
+ * @param {Object} details An object which may have 'imageData',
+ *     'path', or 'tabId' as keys.
+ */
+chrome.browserAction.setIcon = function(details) {};
+
+/**
+ * @param {Object} details An object which has 'title' and optionally
+ *     'tabId'.
+ */
+chrome.browserAction.setTitle = function(details) {};
+
+/** @type {ChromeEvent} */
+chrome.browserAction.onClicked;
+
+
+/** @see http://code.google.com/chrome/extensions/bookmarks.html */
+chrome.bookmarks = {};
+
+/**
+ * @param {Object} bookmark An object which has 'parentId' and
+ *     optionally 'index', 'title', and 'url'.
+ * @param {function(BookmarkTreeNode) : void=} opt_callback The
+ *     callback function which accepts a BookmarkTreeNode object.
+ * @return {BookmarkTreeNode}
+ */
+chrome.bookmarks.create = function(bookmark, opt_callback) {};
+
+/**
+ * @param {(string|Array.<string>)} idOrIdList
+ * @param {function(Array.<BookmarkTreeNode>) : void} callback The
+ *     callback function which accepts an array of BookmarkTreeNode.
+ * @return {Array.<BookmarkTreeNode>}
+ */
+chrome.bookmarks.get = function(idOrIdList, callback) {};
+
+/**
+ * @param {string} id
+ * @param {function(Array.<BookmarkTreeNode>) : void} callback The
+ *     callback function which accepts an array of BookmarkTreeNode.
+ * @return {Array.<BookmarkTreeNode>}
+ */
+chrome.bookmarks.getChildren = function(id, callback) {};
+
+/**
+ * @param {function(Array.<BookmarkTreeNode>) : void} callback The
+ *     callback function which accepts an array of BookmarkTreeNode.
+ * @return {Array.<BookmarkTreeNode>}
+ */
+chrome.bookmarks.getTree = function(callback) {};
+
+/**
+ * @param {string} id
+ * @param {Object} destination An object which has 'parentId' and
+ *     optionally 'index'.
+ * @param {function(Array.<BookmarkTreeNode>) : void=} opt_callback
+ *     The callback function which accepts an array of
+ *     BookmarkTreeNode.
+ * @return {BookmarkTreeNode}
+ */
+chrome.bookmarks.move = function(id, destination, opt_callback) {};
+
+/**
+ * @param {string} id
+ * @param {function() : void=} opt_callback
+ */
+chrome.bookmarks.remove = function(id, opt_callback) {};
+
+/**
+ * @param {string} id
+ * @param {function() : void=} opt_callback
+ */
+chrome.bookmarks.removeTree = function(id, opt_callback) {};
+
+/**
+ * @param {string} query
+ * @param {function(Array.<BookmarkTreeNode>) : void} callback
+ * @return {Array.<BookmarkTreeNode>}
+ */
+chrome.bookmarks.search = function(query, callback) {};
+
+/**
+ * @param {string} id
+ * @param {Object} changes An object which may have 'title' as a key.
+ * @param {function(BookmarkTreeNode) : void=} opt_callback The
+ *     callback function which accepts a BookmarkTreeNode object.
+ * @return {BookmarkTreeNode}
+ */
+chrome.bookmarks.update = function(id, changes, opt_callback) {};
+
+/** @type {ChromeEvent} */
+chrome.bookmarks.onChanged;
+/** @type {ChromeEvent} */
+chrome.bookmarks.onChildrenReordered;
+/** @type {ChromeEvent} */
+chrome.bookmarks.onCreated;
+/** @type {ChromeEvent} */
+chrome.bookmarks.onMoved;
+/** @type {ChromeEvent} */
+chrome.bookmarks.onRemoved;
+
+
+// Classes
+
+/**
+ * @see http://code.google.com/chrome/extensions/tabs.html
+ * @constructor
+ */
+function Tab() {}
+/** @type {number} */
+Tab.prototype.id;
+/** @type {number} */
+Tab.prototype.index;
+/** @type {number} */
+Tab.prototype.windowId;
+/** @type {boolean} */
+Tab.prototype.selected;
+/** @type {string} */
+Tab.prototype.url;
+/** @type {string} */
+Tab.prototype.title;
+/** @type {string} */
+Tab.prototype.favIconUrl;
+/** @type {string} */
+Tab.prototype.status;
+
+/**
+ * @see http://code.google.com/chrome/extensions/windows.html
+ * @constructor
+ */
+function ChromeWindow() {}
+/** @type {number} */
+ChromeWindow.prototype.id;
+/** @type {boolean} */
+ChromeWindow.prototype.focused;
+/** @type {number} */
+ChromeWindow.prototype.top;
+/** @type {number} */
+ChromeWindow.prototype.left;
+/** @type {number} */
+ChromeWindow.prototype.width;
+/** @type {number} */
+ChromeWindow.prototype.height;
+/** @type {Array.<Tab>} */
+ChromeWindow.prototype.tabs;
+
+/**
+ * @see http://code.google.com/chrome/extensions/events.html
+ * @constructor
+ */
+function ChromeEvent() {}
+/** @param {Function} callback */
+ChromeEvent.prototype.addListener = function(callback) {};
+/** @param {Function} callback */
+ChromeEvent.prototype.removeListener = function(callback) {};
+/** @param {Function} callback */
+ChromeEvent.prototype.hasListener = function(callback) {};
+/** @param {Function} callback */
+ChromeEvent.prototype.hasListeners = function(callback) {};
+
+/**
+ * @see http://code.google.com/chrome/extensions/extension.html#type-Port
+ * @constructor
+ */
+function Port() {}
+/** @type {string} */
+Port.prototype.name;
+/** @type {Tab} */
+Port.prototype.tab;
+/** @type {MessageSender} */
+Port.prototype.sender;
+/** @type {ChromeEvent} */
+Port.prototype.onMessage;
+/** @type {ChromeEvent} */
+Port.prototype.onDisconnect;
+/**
+ * @param {Object.<string>} obj Message object.
+ */
+Port.prototype.postMessage = function(obj) {};
+
+/**
+ * @see http://code.google.com/chrome/extensions/extension.html#type-MessageSender
+ * @constructor
+ */
+function MessageSender() {}
+/** @type {Tab} */
+MessageSender.prototype.tab;
+/** @type {string} */
+MessageSender.prototype.id;
+
+/**
+ * @see http://code.google.com/chrome/extensions/bookmarks.html#type-BookmarkTreeNode
+ * @constructor
+ */
+function BookmarkTreeNode() {}
+/** @type {string} */
+BookmarkTreeNode.prototype.id;
+/** @type {string} */
+BookmarkTreeNode.prototype.parentId;
+/** @type {number} */
+BookmarkTreeNode.prototype.index;
+/** @type {string} */
+BookmarkTreeNode.prototype.url;
+/** @type {string} */
+BookmarkTreeNode.prototype.title;
+/** @type {number} */
+BookmarkTreeNode.prototype.dateAdded;
+/** @type {number} */
+BookmarkTreeNode.prototype.dateGroupModified;
+/** @type {Array.<BookmarkTreeNode>} */
+BookmarkTreeNode.prototype.children;
---
+++ test/com/google/javascript/jscomp/CommandLineRunnerTest.java
@@ -0,0 +1,332 @@
+/*
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.javascript.rhino.Node;
+
+import junit.framework.TestCase;
+
+import org.kohsuke.args4j.CmdLineException;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Tests for {@link CommandLineRunner}.
+ *
+ * @author nicksantos@google.com (Nick Santos)
+ */
+public class CommandLineRunnerTest extends TestCase {
+
+  private Compiler lastCompiler = null;
+
+  // If set to true, uses comparison by string instead of by AST.
+  private boolean useStringComparison = false;
+
+  private List<String> args = Lists.newArrayList();
+
+  /** Externs for the test */
+  private final JSSourceFile[] externs = new JSSourceFile[] {
+    JSSourceFile.fromCode("externs",
+        "var arguments;" +
+        "/** @constructor \n * @param {...*} var_args \n " +
+        "* @return {!Array} */ " +
+        "function Array(var_args) {}\n"
+        + "/** @constructor */ function Window() {}\n"
+        + "/** @type {string} */ Window.prototype.name;\n"
+        + "/** @type {Window} */ var window;"
+        + "/** @nosideeffects */ function noSideEffects() {}")
+  };
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    lastCompiler = null;
+    useStringComparison = false;
+    args.clear();
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void testTypeCheckingOffByDefault() {
+    test("function f(x) { return x; } f();",
+         "function f(a) { return a; } f();");
+  }
+
+  public void testTypeCheckingOnWithVerbose() {
+    args.add("--warning_level=VERBOSE");
+    test("function f(x) { return x; } f();", TypeCheck.WRONG_ARGUMENT_COUNT);
+  }
+
+  public void testTypeCheckOverride1() {
+    args.add("--warning_level=VERBOSE");
+    args.add("--jscomp_off=checkTypes");
+    testSame("var x = x || {}; x.f = function() {}; x.f(3);");
+  }
+
+  public void testTypeCheckOverride2() {
+    args.add("--warning_level=DEFAULT");
+    testSame("var x = x || {}; x.f = function() {}; x.f(3);");
+
+    args.add("--jscomp_warning=checkTypes");
+    test("var x = x || {}; x.f = function() {}; x.f(3);",
+         TypeCheck.WRONG_ARGUMENT_COUNT);
+  }
+
+  public void testCheckSymbolsOffForDefault() {
+    args.add("--warning_level=DEFAULT");
+    test("x = 3; var y; var y;", "x=3; var y;");
+  }
+
+  public void testCheckSymbolsOnForVerbose() {
+    args.add("--warning_level=VERBOSE");
+    test("x = 3;", VarCheck.UNDEFINED_VAR_ERROR);
+    test("var y; var y;", SyntacticScopeCreator.VAR_MULTIPLY_DECLARED_ERROR);
+  }
+
+  public void testCheckSymbolsOverrideForVerbose() {
+    args.add("--warning_level=VERBOSE");
+    args.add("--jscomp_off=undefinedVars");
+    testSame("x = 3;");
+  }
+
+  public void testCheckUndefinedProperties() {
+    args.add("--warning_level=VERBOSE");
+    args.add("--jscomp_error=missingProperties");
+    test("var x = {}; var y = x.bar;", TypeCheck.INEXISTENT_PROPERTY);
+  }
+
+  public void testDuplicateParams() {
+    test("function (a, a) {}", RhinoErrorReporter.DUPLICATE_PARAM);
+    assertTrue(lastCompiler.hasHaltingErrors());
+  }
+
+  public void testDefineFlag() {
+    args.add("--define=FOO");
+    args.add("--define=\"BAR=5\"");
+    args.add("--D"); args.add("CCC");
+    args.add("-D"); args.add("DDD");
+    test("/** @define {boolean} */ var FOO = false;" +
+         "/** @define {number} */ var BAR = 3;" +
+         "/** @define {boolean} */ var CCC = false;" +
+         "/** @define {boolean} */ var DDD = false;",
+         "var FOO = true, BAR = 5, CCC = true, DDD = true;");
+  }
+
+  public void testScriptStrictModeNoWarning() {
+    test("'use strict';", "");
+    test("'no use strict';", CheckSideEffects.USELESS_CODE_ERROR);
+  }
+
+  public void testFunctionStrictModeNoWarning() {
+    test("function f() {'use strict';}", "function f() {}");
+    test("function f() {'no use strict';}",
+         CheckSideEffects.USELESS_CODE_ERROR);
+  }
+
+  public void testQuietMode() {
+    args.add("--warning_level=DEFAULT");
+    test("/** @type { not a type name } */ var x;",
+         RhinoErrorReporter.PARSE_ERROR);
+    args.add("--warning_level=QUIET");
+    testSame("/** @type { not a type name } */ var x;");
+  }
+
+  public void testProcessClosurePrimitives() {
+    test("var goog = {}; goog.provide('goog.dom');",
+         "var goog = {}; goog.dom = {};");
+    args.add("--process_closure_primitives=false");
+    testSame("var goog = {}; goog.provide('goog.dom');");
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Integration tests
+
+  public void testIssue70() {
+    test("function foo({}) {}", RhinoErrorReporter.PARSE_ERROR);
+  }
+
+  public void testIssue81() {
+    args.add("--compilation_level=ADVANCED_OPTIMIZATIONS");
+    useStringComparison = true;
+    test("eval('1'); var x = eval; x('2');",
+         "eval(\"1\");(0,eval)(\"2\");");
+  }
+
+  public void testIssue115() {
+    args.add("--compilation_level=SIMPLE_OPTIMIZATIONS");
+    args.add("--warning_level=VERBOSE");
+    test("function f() { " +
+         "  var arguments = Array.prototype.slice.call(arguments, 0);" +
+         "  return arguments[0]; " +
+         "}",
+         "function f() { " +
+         "  arguments = Array.prototype.slice.call(arguments, 0);" +
+         "  return arguments[0]; " +
+         "}");
+  }
+
+  public void testDebugFlag1() {
+    args.add("--compilation_level=SIMPLE_OPTIMIZATIONS");
+    args.add("--debug=false");
+    testSame("function foo(a) {}");
+  }
+
+  public void testDebugFlag2() {
+    args.add("--compilation_level=SIMPLE_OPTIMIZATIONS");
+    args.add("--debug=true");
+    test("function foo(a) {}",
+         "function foo($a$$) {}");
+  }
+
+  public void testDebugFlag3() {
+    args.add("--compilation_level=ADVANCED_OPTIMIZATIONS");
+    args.add("--warning_level=QUIET");
+    args.add("--debug=false");
+    test("function Foo() {};" +
+         "Foo.x = 1;" +
+         "function f() {throw new Foo().x;} f();",
+         "function a() {};" +
+         "throw new a().a;");
+  }
+
+  public void testDebugFlag4() {
+    args.add("--compilation_level=ADVANCED_OPTIMIZATIONS");
+    args.add("--warning_level=QUIET");
+    args.add("--debug=true");
+    test("function Foo() {};" +
+        "Foo.x = 1;" +
+        "function f() {throw new Foo().x;} f();",
+        "function $Foo$$() {};" +
+        "throw new $Foo$$().$x$;");
+  }
+
+  /* Helper functions */
+
+  private void testSame(String original) {
+    testSame(new String[] { original });
+  }
+
+  private void testSame(String[] original) {
+    test(original, original);
+  }
+
+  private void test(String original, String compiled) {
+    test(new String[] { original }, new String[] { compiled });
+  }
+
+  /**
+   * Asserts that when compiling with the given compiler options,
+   * {@code original} is transformed into {@code compiled}.
+   */
+  private void test(String[] original, String[] compiled) {
+    Compiler compiler = compile(original);
+    assertEquals("Expected no warnings or errors\n" +
+        "Errors: \n" + Joiner.on("\n").join(compiler.getErrors()) +
+        "Warnings: \n" + Joiner.on("\n").join(compiler.getWarnings()),
+        0, compiler.getErrors().length + compiler.getWarnings().length);
+
+    Node root = compiler.getRoot().getLastChild();
+    if (useStringComparison) {
+      assertEquals(Joiner.on("").join(compiled), compiler.toSource());
+    } else {
+      Node expectedRoot = parse(compiled);
+      String explanation = expectedRoot.checkTreeEquals(root);
+      assertNull("\nExpected: " + compiler.toSource(expectedRoot) +
+          "\nResult: " + compiler.toSource(root) +
+          "\n" + explanation, explanation);
+    }
+  }
+
+  /**
+   * Asserts that when compiling, there is an error or warning.
+   */
+  private void test(String original, DiagnosticType warning) {
+    test(new String[] { original }, warning);
+  }
+
+  /**
+   * Asserts that when compiling, there is an error or warning.
+   */
+  private void test(String[] original, DiagnosticType warning) {
+    Compiler compiler = compile(original);
+    assertEquals("Expected exactly one warning or error " +
+        "Errors: \n" + Joiner.on("\n").join(compiler.getErrors()) +
+        "Warnings: \n" + Joiner.on("\n").join(compiler.getWarnings()),
+        1, compiler.getErrors().length + compiler.getWarnings().length);
+    if (compiler.getErrors().length > 0) {
+      assertEquals(warning, compiler.getErrors()[0].getType());
+    } else {
+      assertEquals(warning, compiler.getWarnings()[0].getType());
+    }
+  }
+
+  private Compiler compile(String original) {
+    return compile( new String[] { original });
+  }
+
+  private Compiler compile(String[] original) {
+    String[] argStrings = args.toArray(new String[] {});
+    CommandLineRunner runner = null;
+    try {
+      runner = new CommandLineRunner(argStrings);
+    } catch (CmdLineException e) {
+      throw new RuntimeException(e);
+    }
+    Compiler compiler = runner.createCompiler();
+    lastCompiler = compiler;
+    JSSourceFile[] inputs = new JSSourceFile[original.length];
+    for (int i = 0; i < original.length; i++) {
+      inputs[i] = JSSourceFile.fromCode("input" + i, original[i]);
+    }
+    CompilerOptions options = runner.createOptions();
+    try {
+      runner.setRunOptions(options);
+    } catch (AbstractCommandLineRunner.FlagUsageException e) {
+      fail("Unexpected exception " + e);
+    } catch (IOException e) {
+      assert(false);
+    }
+    compiler.compile(
+        externs, CompilerTestCase.createModuleChain(original), options);
+    return compiler;
+  }
+
+  private Node parse(String[] original) {
+    String[] argStrings = args.toArray(new String[] {});
+    CommandLineRunner runner = null;
+    try {
+      runner = new CommandLineRunner(argStrings);
+    } catch (CmdLineException e) {
+      throw new RuntimeException(e);
+    }
+    Compiler compiler = runner.createCompiler();
+    JSSourceFile[] inputs = new JSSourceFile[original.length];
+    for (int i = 0; i < inputs.length; i++) {
+      inputs[i] = JSSourceFile.fromCode("input" + i, original[i]);
+    }
+    compiler.init(externs, inputs, new CompilerOptions());
+    Node all = compiler.parseInputs();
+    Node n = all.getLastChild();
+    return n;
+  }
+}
